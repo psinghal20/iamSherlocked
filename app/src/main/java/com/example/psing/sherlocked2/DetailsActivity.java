b41884e1100a45c29a8114dd176e998a5275ae0a
psinghal20 <psinghal20@gmail.com>
2017-01-06 18:51:00 +0530

    Fixed table 3 error

--- app/src/main/java/com/example/psing/sherlocked2/DetailsActivity.java
+++ app/src/main/java/com/example/psing/sherlocked2/DetailsActivity.java
@@ -5,10 +5,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
     TextView t1,t2,t3,t4;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,7 @@ public class DetailsActivity extends AppCompatActivity {
         t3 = (TextView) findViewById(R.id.textView11);
         t4 = (TextView) findViewById(R.id.textView12);
         t1 = (TextView) findViewById(R.id.textView9);
+        imageView=(ImageView)findViewById(R.id.imageView2);
         Cursor res = null;
         switch (SeasonActivity.Item) {
             case 0: {
@@ -29,6 +32,59 @@ public class DetailsActivity extends AppCompatActivity {
             }
             case 2: {
                 res = SeasonActivity.mHelper.getalldata(TaskContract.TaskEntry.TABLE3);
+                break;
+            }
+        }
+        switch (SeasonActivity.Item){
+            case 0:{
+                    switch (EpisodeActivity.pos){
+                        case 0:{
+                            imageView.setImageResource(R.drawable.s1e1n);
+                            break;
+                        }
+                        case 1:{
+                            imageView.setImageResource(R.drawable.s1e2);
+                            break;
+                        }
+                        case 2:{
+                            imageView.setImageResource(R.drawable.s1e3);
+                            break;
+                        }
+                    }
+                break;
+            }
+            case 1:{
+                switch (EpisodeActivity.pos){
+                    case 0:{
+                        imageView.setImageResource(R.drawable.s2e1);
+                        break;
+                    }
+                    case 1:{
+                        imageView.setImageResource(R.drawable.s2e2);
+                        break;
+                    }
+                    case 2:{
+                        imageView.setImageResource(R.drawable.s2e3);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 2: {
+                switch (EpisodeActivity.pos) {
+                    case 0: {
+                        imageView.setImageResource(R.drawable.s3e1);
+                        break;
+                    }
+                    case 1: {
+                        imageView.setImageResource(R.drawable.s3e2);
+                        break;
+                    }
+                    case 2: {
+                        imageView.setImageResource(R.drawable.s3e3);
+                        break;
+                    }
+                }
                 break;
             }
         }
